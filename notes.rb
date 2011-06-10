f15bd07832c4a55ad291f4423627c127e8688360
Brian Kelly <polymonic@gmail.com>
2011-06-10 15:35:45 -0400

    Add RSS feed.

--- notes.rb
+++ notes.rb
@@ -2,21 +2,53 @@ require 'bundler/setup'
 require 'sinatra'
 require 'rdiscount'
 require 'erb'
+require 'builder'
 
 set :markdown, :layout_engine => :erb
 
-get '/' do
-  @topics = nil
+helpers do
+  def getTopics()
+    @topics = nil
 
-  Dir.chdir(File.dirname(__FILE__) + "/views/") do
-    @topics = Dir.glob("*.md")
+     Dir.chdir(File.dirname(__FILE__) + "/views/") do
+       @topics = Dir.glob("*.md")
+     end
+
+     @topics.reject! {|entry| entry == "index.md"}
+     @topics.collect! {|entry| entry.split(".")[0]}
+     @topics.sort!
   end
+end
 
-  @topics.reject! {|entry| entry == "index.md"}
-  @topics.collect! {|entry| entry.split(".")[0]}
-  @topics.sort!
+get '/' do
+  @topics = getTopics
   
   erb :index
+end
+
+get '/rss' do
+ @topics = getTopics
+  
+  builder do |xml|
+    xml.instruct! :xml, :version => '1.0'
+    xml.rss :version => "2.0" do
+      xml.channel do
+        xml.title "Build & Deploy Notes"
+        xml.description "Build & Deploy Development Notes"
+        xml.link "http://notes.buildndeploy.com/"
+
+        @topics.each do |topic|
+          xml.item do
+            xml.title topic
+            xml.link "http://notes.buildndeploy.com/#{topic}"
+            xml.description "My notes about #{topic}"
+            xml.pubDate Time.now.rfc822()
+            xml.guid "http://notes.buildndeploy.com/#{topic}"
+          end
+        end
+      end
+    end
+  end
 end
 
 get '/source' do
@@ -35,3 +67,5 @@ end
 get '/:topic/source' do
   send_file File.dirname(__FILE__) + "/views/#{params[:topic]}.md", :type => :text
 end
+
+
